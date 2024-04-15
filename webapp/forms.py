1be6e4a6fa6d849004fed0203da0bb1b89148fa5
Riyan Jamil <riyanjamil220@gmail.com>
2024-04-15 08:22:06 +0500

    created form classes in forms.py

---
+++ webapp/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm ,AuthenticationForm
+from django.contrib.auth.models import User
+
+from django import forms
+from django.forms.widgets import PasswordInput,TextInput
+
+
+# register a user
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+        
+
+# login a user
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget = TextInput())
+    password = forms.CharField(widget = PasswordInput())
